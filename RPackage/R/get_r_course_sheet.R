e1e6b4722bac5336b01ffb2a5340683f4204716d
Måns Magnusson <mons.magnusson@gmail.com>
2017-09-01 13:20:39 +0200

    Change package

--- RPackage/R/get_r_course_sheet.R
+++ RPackage/R/get_r_course_sheet.R
@@ -3,7 +3,6 @@
 #' @details Get the R course sheet from Google sheets.
 #'
 get_r_course_gsheet <- function(){
-  gs_obj <- googlesheets::gs_key(x = "1urpVyCVr3w_hFlTkccKkYPhzf9SpLA_k8fWbcUwUlhE", verbose = FALSE)
-  gs_df <- suppressMessages(googlesheets::gs_read(gs_obj, verbose = FALSE))
+  gs_df <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1urpVyCVr3w_hFlTkccKkYPhzf9SpLA_k8fWbcUwUlhE')
   gs_df
 }
