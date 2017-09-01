10f7cacd5eaf2a7188d1023f32c66717022b5066
Måns Magnusson <mons.magnusson@gmail.com>
2017-09-01 12:55:43 +0200

    Fix bugs

--- R/install_all_packages.R
+++ R/install_all_packages.R
@@ -1,3 +1,3 @@
-install.packages("devtools")
+install.packages("devtools", repos = "https://cloud.r-project.org")
 devtools::install_github("STIMALiU/ComputerLabs", subdir = "RPackage")
 stimaRpackages::install_stima_packages()
--- tests/travis_tests.sh
+++ tests/travis_tests.sh
@@ -5,12 +5,11 @@ git log -n 1
 R --version
 script_start_time=$(date +"%T")
 # Set STIMA Error to 1 (to break Travis)
-echo "STIMA=1" > "STIMA.sh"
+echo "STIMA_ERROR=1" > "STIMA_ERROR.sh"
 
 
 echo $(date +"%T")
 
-echo " " && echo -en "travis_fold:start:install_all_packages\n"
 bash R/bash/install_all_packages.sh
 tr_exit=$?; echo $tr_exit; if [[ $tr_exit != 0 ]]; then exit $tr_exit; fi
 
